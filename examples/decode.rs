3246eff058a5af83f8e647bb892c120283310047
luozijun <luozijun.assistant@gmail.com>
2018-09-30 20:23:38 +0800

    update examples/decode.rs

--- examples/decode.rs
+++ examples/decode.rs
@@ -1,23 +1,27 @@
+#![feature(duration_as_u128)]
+
 extern crate openh264;
 extern crate mp4;
 
 use openh264::Decoder;
-use mp4::track::H264VideoTrack;
 use mp4::track::VideoCodec;
 use mp4::nalu::Nalu;
 
+use std::time;
 use std::io::Write;
-use std::io::Read;
-use std::io::Seek;
 use std::fs::{ self, OpenOptions, };
 
 
-fn decode_nalu<F: Write>(decoder: &mut Decoder, nalu: &Nalu, output: &mut F) {
+fn decode_nalu<F: Write>(decoder: &mut Decoder, nalu: &Nalu, output: &mut F, frams_count: &mut usize, elapsed: &mut u128) {
     let bytes = nalu.as_bytes();
+
+    let now = time::Instant::now();
 
     match decoder.decode(bytes) {
         Some(mut i420_frame) => {
             i420_frame.save(output);
+            *frams_count += 1;
+            *elapsed += now.elapsed().as_millis();
         },
         None => {
 
@@ -35,6 +39,9 @@ fn main() {
     let mut mp4_file = fs::File::open("a.mp4").unwrap();  
     let mp4_ctx = mp4::parse::parse(&mut mp4_file).unwrap();
     
+    let mut frams_count = 0usize;
+    let mut elapsed: u128 = 0;
+
     for video_track in mp4_ctx.video_tracks {
         if video_track.codec() == VideoCodec::H264 {
             println!("{}", video_track);
@@ -43,21 +50,22 @@ fn main() {
 
             for sps in avc_config.sps.iter() {
                 let nalu = Nalu::new(sps.clone());
-                decode_nalu(&mut decoder, &nalu, &mut rawvideo_file);
+                decode_nalu(&mut decoder, &nalu, &mut rawvideo_file, &mut frams_count, &mut elapsed);
             }
             for pps in avc_config.pps.iter() {
                 let nalu = Nalu::new(pps.clone());
-                decode_nalu(&mut decoder, &nalu, &mut rawvideo_file);
+                decode_nalu(&mut decoder, &nalu, &mut rawvideo_file, &mut frams_count, &mut elapsed);
             }
 
             for sample in video_track.samples() {
                 for nalu in sample.nalus(&mut mp4_file) {
-                    decode_nalu(&mut decoder, &nalu, &mut rawvideo_file);
+                    decode_nalu(&mut decoder, &nalu, &mut rawvideo_file, &mut frams_count, &mut elapsed);
                 }
             }
         }
     }
 
+    println!("Frames: {} Duration: {}ms", frams_count, elapsed);
     println!("ffplay -f rawvideo -pixel_format yuv420p -video_size 1280x720 rawvideo.yuv");
     println!("vlc --demux rawvideo --rawvid-fps 5 --rawvid-width 1280 --rawvid-height 720 --rawvid-chroma I420 rawvideo.yuv");
 }
